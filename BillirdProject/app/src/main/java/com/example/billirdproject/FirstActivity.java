337a3140883a053b105ac0b92ca9f1087753eb90
rltk2842 <rltk2842@gmail.com>
2019-05-14 15:10:41 +0900

    no message

--- BillirdProject/app/src/main/java/com/example/billirdproject/FirstActivity.java
+++ BillirdProject/app/src/main/java/com/example/billirdproject/FirstActivity.java
@@ -1,13 +1,40 @@
 package com.example.billirdproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+    Button btnLo, btnMem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        btnMem = findViewById(R.id.btnMem);
+
+        btnMem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FirstActivity.this, MainActivity.class);
+
+                startActivity(it);
+            }
+        });
+
+        btnLo = findViewById(R.id.btnLo);
+
+        btnLo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent it = new Intent(FirstActivity.this,Main2Activity.class);
+//
+//                startActivity(it);
+            }
+        });
+
     }
 }
